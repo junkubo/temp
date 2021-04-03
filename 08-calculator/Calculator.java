55baae3f9bfd104cc193fc5da19197478724ceef
Jun <jkubo20@stuy.edu>
2021-04-02 20:14:52 -0400

    test

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -1,8 +1,33 @@
 public class Calculator{
+
   public static double eval(String s){
+    Double first;
+    Double second;
     String[] str = s.split(" ", 0);
+    MyDeque<String> arr = new <String> MyDeque(20);
     for (int i = 0; i < str.length; i++) {
-      System.out.println(str[i]);
+      try {
+        Double.parseDouble(str[i]);
+        arr.addLast(str[i]);
+        //System.out.println(arr.toString());
+      } catch (NumberFormatException nfe) {
+        second = Double.parseDouble(arr.removeLast());
+        first = Double.parseDouble(arr.removeLast());
+        if (str[i] == "+") {
+          arr.addLast(Double.toString(first + second));
+      } else if (str[i] == "-") {
+          arr.addLast(Double.toString(first - second));
+      } else if (str[i] == "*") {
+          arr.addLast(Double.toString(first * second));
+      } else if (str[i] == "/") {
+          arr.addLast(Double.toString(first / second));
+      } else if (str[i] == "%") {
+          arr.addLast(Double.toString(first % second));
+      } else {
+        //  throw error here...
+      }
+
+      }
     }
     return 0;
   }
--- 08-calculator/Driver.java
+++ 08-calculator/Driver.java
@@ -1,6 +1,6 @@
 public class Driver {
   public static void main (String[] args) {
     Calculator c = new Calculator();
-    c.eval("t e s t 1 2 3 4");
+    c.eval("1 2 +");
   }
 }
